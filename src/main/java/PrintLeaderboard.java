e1850948c594d8d059b01a420c88d2629f535804
Aaron Bester <bester@uoguelph.ca>
2020-12-11 16:12:59 -0500

    changed location of files

---
+++ src/main/java/PrintLeaderboard.java
@@ -0,0 +1,16 @@
+
+package leaderboard;
+
+/**
+*Class that holds the main function.
+*/
+public class PrintLeaderboard {
+/**
+*Main that prints out the score and player id in the given file.
+*@param args Command line arguments.
+*/
+  public static void main(String[] args) {
+    ScoreCalc scoreCalculator = new ScoreCalc("sampleInput.txt");
+    System.out.println(scoreCalculator.toString());
+  }
+}
